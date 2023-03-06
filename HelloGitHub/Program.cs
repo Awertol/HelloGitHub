ec8ff552d82090a3ee96523a3093e27631bc67a9
Michal Helgert <michal.helgert@seznam.cz>
2023-03-06 10:45:58 +0100

    Přidána barva textu, rozšířeno pole zájmů

--- HelloGitHub/Program.cs
+++ HelloGitHub/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Ahoj, jsem Michal Helgert, chodím na PedF UK a baví mě: ");
 
-string[] zajmy = { "Netflix", "Výměna manželek", "PlayStation", "Spánek" };
+Console.ForegroundColor = ConsoleColor.Green;
+
+string[] zajmy = { "Netflix", "Výměna manželek", "PlayStation", "Spánek", "Hardware" };
 foreach(var item in zajmy)
 {
     Console.WriteLine(item);
