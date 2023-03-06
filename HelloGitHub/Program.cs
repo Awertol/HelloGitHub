a1b41aa9238f76501193019a65668306642ddd74
Michal Helgert <michal.helgert@seznam.cz>
2023-03-06 10:42:03 +0100

    Přidejte soubory projektu.

---
+++ HelloGitHub/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ahoj, jsem Michal Helgert, chodím na PedF UK a baví mě: ");
+
+string[] zajmy = { "Netflix", "Výměna manželek", "PlayStation", "Spánek" };
+foreach(var item in zajmy)
+{
+    Console.WriteLine(item);
+}
